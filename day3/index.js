0d4a30d14a63800b14da58589e53dedb505cd72f
Linus Almgren <linus.almgren@gmail.com>
2024-12-03 21:18:54 +0100

    solve day3p2

--- day3/index.js
+++ day3/index.js
@@ -2,12 +2,25 @@ import { open } from "node:fs/promises";
 
 const input = await open("./input.txt");
 
+let active = true;
+
 function getLineValue(input) {
-  const valid = input.match(/mul\(\d+,\d+\)/g);
+  const valid = input.match(/(?:mul\(\d+,\d+\)|do(?:n't)?\(\))/g);
 
   let sum = 0;
 
   for (const operation of valid) {
+    if (operation === "do()") {
+      active = true;
+      continue;
+    }
+
+    if (operation === "don't()") {
+      active = false;
+      continue;
+    }
+
+    if (!active) continue;
     const [num1, num2] = operation.match(/\d+/g);
     sum += +num1 * +num2;
   }
