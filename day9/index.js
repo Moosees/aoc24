7a7adae8d095375432c2de68d251c70836ba34ed
Linus Almgren <linus.almgren@gmail.com>
2024-12-10 00:36:58 +0100

    fail day9p2

--- day9/index.js
+++ day9/index.js
@@ -33,4 +33,94 @@ while (i < disk.length) {
 }
 
 // console.log(disk.join(""));
-console.log(part1);
+console.log({ part1 });
+
+// PART 2
+const input2 = await open("./input.txt");
+
+const disk2 = [];
+const free = { 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [] };
+
+for await (const line of input2.readLines()) {
+  let id = 0;
+  let diskIndex = 0;
+
+  for (let i = 0; i < line.length; i++) {
+    const length = +line[i];
+    const isFile = i % 2 === 0;
+    const blocks = new Array(length).fill(isFile ? id : ".");
+
+    disk2.push(...blocks);
+    if (isFile) id++;
+    else if (length > 0) free[length].push(diskIndex);
+
+    diskIndex += length;
+  }
+}
+
+function findLength(startIndex) {
+  let length = 0;
+  let i = startIndex;
+  const id = disk2[i];
+  while (disk2[i] === id) {
+    length++;
+    i--;
+  }
+  return length;
+}
+
+function findFree(length, fromIndex) {
+  let foundLength;
+  for (let i = 1; i <= 9; i++) {
+    if (i < length) continue;
+    if (
+      free[i].length > 0 &&
+      (foundLength === undefined || free[i][0] < foundLength)
+    ) {
+      foundLength = i;
+    }
+  }
+
+  if (!foundLength || free[foundLength][0] > fromIndex - length) return;
+
+  const toIndex = free[foundLength].shift();
+  const diff = +foundLength - length;
+
+  if (diff > 0) {
+    const diffIndex = toIndex + length;
+    free[diff] = [...free[diff], diffIndex].sort((a, b) => a - b);
+  }
+
+  return toIndex;
+}
+
+function move(fromIndex, toIndex, length) {
+  let done = 0;
+  const id = disk2[fromIndex];
+
+  while (done < length) {
+    disk2[fromIndex - done] = ".";
+    disk2[toIndex + done] = id;
+    done++;
+  }
+}
+
+let prevId = ".";
+for (let i = disk2.length - 1; i >= 0; i--) {
+  if (disk2[i] === prevId || disk2[i] === ".") continue;
+
+  const length = findLength(i);
+  const found = findFree(length, i);
+  if (found !== undefined) {
+    move(i, found, length);
+  }
+
+  prevId = disk2[i];
+}
+
+const part2 = disk2.reduce((acc, id, i) => {
+  if (id === ".") return acc;
+  return acc + id * i;
+}, 0);
+
+console.log({ part2 });
