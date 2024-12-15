ad4591f919978812102cc09a50d961197096d427
Linus Almgren <linus.almgren@gmail.com>
2024-12-15 01:28:25 +0100

    solve day14p2

--- day14/index.js
+++ day14/index.js
@@ -6,15 +6,21 @@ const input = await open("./input.txt");
 const dimensions = { x: 101, y: 103 };
 const quads = [0, 0, 0, 0];
 
-for await (const line of input.readLines()) {
+function findPosAtSecond(line, s) {
   const [start, velocity] = line.split(" ");
   const [startX, startY] = start.split("=")[1].split(",");
   const [velX, velY] = velocity.split("=")[1].split(",");
 
-  const movedX = (+startX + velX * 100) % dimensions.x;
-  const movedY = (+startY + velY * 100) % dimensions.y;
+  const movedX = (+startX + velX * s) % dimensions.x;
+  const movedY = (+startY + velY * s) % dimensions.y;
   const finalX = movedX >= 0 ? Math.abs(movedX) : movedX + dimensions.x;
   const finalY = movedY >= 0 ? Math.abs(movedY) : movedY + dimensions.y;
+
+  return { finalX, finalY };
+}
+
+for await (const line of input.readLines()) {
+  const { finalX, finalY } = findPosAtSecond(line, 100);
 
   if (
     finalX === Math.floor(dimensions.x / 2) ||
@@ -35,3 +41,54 @@ const part1 = quads.reduce((sum, val) => sum * val);
 console.timeEnd("Time");
 console.log("Answer: ", part1);
 console.log("------------------");
+
+console.log("Part two");
+async function paintMapAtSecond(s) {
+  const map = new Array(103)
+    .fill(undefined)
+    .map((_x) => new Array(101).fill(0));
+
+  const input = await open("./input.txt");
+
+  for await (const line of input.readLines()) {
+    const { finalX, finalY } = findPosAtSecond(line, s);
+    map[finalY][finalX]++;
+  }
+
+  map.forEach((line) => console.log(line.join("").replace(/0/g, " ")));
+}
+
+function getVarianceAtSecond(lines, s) {
+  let variance = 0;
+
+  lines.forEach((line) => {
+    const { finalX, finalY } = findPosAtSecond(line, s);
+    variance += Math.abs(finalX - 50) + Math.abs(finalY - 50);
+  });
+
+  return variance;
+}
+
+async function lookForTrees() {
+  const lines = [];
+  const p2input = await open("./input.txt");
+
+  for await (const line of p2input.readLines()) {
+    lines.push(line);
+  }
+
+  let minVarianceFound;
+  for (let i = 0; i < 9999; i++) {
+    const variance = getVarianceAtSecond(lines, i);
+
+    if (i === 0 || variance < minVarianceFound) {
+      minVarianceFound = variance;
+      console.log({ s: i, variance });
+    }
+  }
+}
+
+console.time("Part 2");
+await lookForTrees();
+console.timeEnd("Part 2");
+paintMapAtSecond(8179);
